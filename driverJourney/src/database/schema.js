fe223b742c6ca0cb8e20e9f3f71c2f369da8f5e7
felipesmendes <contato@felipems.com.br>
2025-04-03 09:21:01 -0300

    Add driverJourney/src/database/schema.js 1743682857467

---
+++ driverJourney/src/database/schema.js
@@ -0,0 +1,20 @@
+import { appSchema, tableSchema } from '@nozbe/watermelondb';
+
+export default appSchema({
+  version: 1,
+  tables: [
+    tableSchema({
+      name: 'pontos',
+      columns: [
+        { name: 'motorista_id', type: 'string' },
+        { name: 'veiculo_id', type: 'string' },
+        { name: 'data_hora', type: 'number' },
+        { name: 'data_hora_fim', type: 'number', isOptional: true },
+        { name: 'tempo', type: 'number' },
+        { name: 'tipo', type: 'string' },
+        { name: 'created_at', type: 'number' },
+        { name: 'updated_at', type: 'number' },
+      ],
+    }),
+  ],
+});
