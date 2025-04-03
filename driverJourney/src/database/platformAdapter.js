ff8f9c01957316e23b32d6f587250530063cd331
felipesmendes <contato@felipems.com.br>
2025-04-03 10:16:36 -0300

    Add driverJourney/src/database/platformAdapter.js 1743686192402

--- driverJourney/src/database/platformAdapter.js
+++ driverJourney/src/database/platformAdapter.js
@@ -1,9 +1,57 @@
 import { Platform } from 'react-native';
 import webStorage from './webStorage';
 
+// Create a wrapper to ensure storage is initialized
+class StorageWrapper {
+  constructor(storage) {
+    this.storage = storage;
+    this.initialized = false;
+    this.initPromise = null;
+  }
+
+  async ensureInitialized() {
+    if (this.initialized) return;
+    if (!this.initPromise) {
+      this.initPromise = this.storage.initStorage();
+    }
+    await this.initPromise;
+    this.initialized = true;
+  }
+
+  async createPonto(...args) {
+    await this.ensureInitialized();
+    return this.storage.createPonto(...args);
+  }
+
+  async getPontosMotorista(...args) {
+    await this.ensureInitialized();
+    return this.storage.getPontosMotorista(...args);
+  }
+
+  async getPontoAtivo(...args) {
+    await this.ensureInitialized();
+    return this.storage.getPontoAtivo(...args);
+  }
+
+  async finalizarPonto(...args) {
+    await this.ensureInitialized();
+    return this.storage.finalizarPonto(...args);
+  }
+
+  async getUnsyncedPontos(...args) {
+    await this.ensureInitialized();
+    return this.storage.getUnsyncedPontos(...args);
+  }
+
+  async markAsSynced(...args) {
+    await this.ensureInitialized();
+    return this.storage.markAsSynced(...args);
+  }
+}
+
 // Export the appropriate storage adapter based on platform
 export const DatabaseService = Platform.select({
-  web: webStorage,
+  web: new StorageWrapper(webStorage),
   default: {
     // Default implementation (could be extended for native platforms)
     createPonto: async () => {
